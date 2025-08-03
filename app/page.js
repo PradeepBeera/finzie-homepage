3c01e46e8e5f8711e3cdfadb93c9f66537be1ec7
PradeepBeera <pradeepbeera87@gmail.com>
2025-08-03 19:13:56 +0530

    Fix: folder name case mismatch for Vercel

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Hero from './components/Hero';
 import Features from './components/Features';
 import About from './components/About';
 import Testimonials from './components/Testimonials';
-import Footer from './Components/Footer';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
